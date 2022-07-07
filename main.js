4fd0786a217aaf75e96ad7c68d91d948eb97ee27
Bjorn Pagen <11238136+bjornpagen@users.noreply.github.com>
2022-07-07 18:28:18 +0000

    add local branch selector

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
 // puppeteer-extra is a drop-in replacement for puppeteer,
 // it augments the installed puppeteer with plugin functionality
-const puppeteer = require('puppeteer-extra')
+const puppeteer = require('puppeteer-extra');
 
 // add stealth plugin and use defaults (all evasion techniques)
 const StealthPlugin = require('puppeteer-extra-plugin-stealth')
-puppeteer.use(StealthPlugin())
+puppeteer.use(StealthPlugin());
 
 // puppeteer usage as normal
 puppeteer.launch({ headless: true }).then(async browser => {
-  console.log('Running tests..')
-  const page = await browser.newPage()
-  await page.goto('https://example.com')
-  await page.waitForTimeout(5000)
-  await page.screenshot({ path: 'testresult.png', fullPage: true })
-  await browser.close()
-  console.log(`All done, check the screenshot.`)
+  const page = await browser.newPage();
+  await page.goto('https://dmvapp.nv.gov/qmaticwebbooking/#/');
+  await page.waitForTimeout(5000);
+  const localBranchName = `Decatur`;
+  const localBranch = (await page.$x(`//div[text()="${localBranchName}"]`))[0];
+  await localBranch.click();
+  await page.waitForTimeout(5000);
+  await page.screenshot({ path: 'testresult.png', fullPage: true });
+  await browser.close();
+  console.log(`All done, check the screenshot.`);
 })
